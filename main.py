2f7fb77c19e855ec182cc8334ea27ea0aed42e5f
GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-03-05 12:29:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,24 +17,37 @@ colorama.init(autoreset=True)
     
 
     
-def convert(file):
+def convert(file, destination):
     from PIL import Image
     import os
-    new = os.path.basename(file).split(".")[0]+".png"
+    if not destination:
+        new = os.path.basename(file).split(".")[0]+".png"
+        
+    else:
+        #print("r")
+        new = destination+os.path.basename(file).split(".")[0]+".png"
     Image.open(file).save(new, format="png")
     
 
     
 
-if len(argv)==2:
+if len(argv)>1:
     file = argv[1]
 
     
 else:
     file = input("Glissez le fichier ici .\n")
 
+des = False
+if len(argv)>2:
+    des = argv[2]
+
+
+
+
+
 print(Fore.GREEN+"Conversion en cours ...")
-convert(file)
+convert(file, des)
 print(Fore.RED+"Conversion terminé !!")
 import time
 time.sleep(5)
